e5da093565f0824d9b6be39d7690fdfcc596d621
tomalikem <58105407+tomalikem@users.noreply.github.com>
2020-01-14 22:53:31 +0100

    deleting trash

--- MongoDB/zad3.js
+++
@@ -1,7 +0,0 @@
-function get_business_of_category(category)
-{
-    return db.buisiness.find({categories: {"$in" : [category]}})
-}
-var tips = get_business_of_category("Doctors")
-tips.forEach(function(doc){print(doc)}
-)
